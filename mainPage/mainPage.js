e8eb7e5d89433373a30a5a3dd0b4920e2b0fcc2f
Niels <niels.debacker03@gmail.com>
2021-12-15 17:55:01 +0100

    test

---
+++ mainPage/mainPage.js
@@ -0,0 +1,31 @@
+document.getElementById("button").addEventListener("click", loadPage);
+
+function loadPage(){
+    var array_Pages = createArrayOfPages();
+    var page = getRandomPage(array_Pages);
+    window.location.replace(page);
+}
+
+function createArrayOfPages(){
+    var array_Pages = ["giveUp", "mainPage", "dontMatter"];//get pages
+    var newArray = spliceMainPage(array_Pages);
+    return newArray;
+}
+
+function getRandomPage(array_pages){
+    alert(array_pages);
+    var amountOfPages = array_pages.length;
+    alert(amountOfPages);
+    var randomNumber = Math.floor((Math.random()*amountOfPages) + 1);
+    var page = array_pages[randomNumber];
+    var link = "../"+ page + "/index.html"; 
+    
+    return link;
+}
+
+function spliceMainPage(array_Pages){
+    var mainPageExists = array_Pages.indexOf('mainPage');
+    if (mainPageExists > -1){
+        array_Pages.splice("mainPage", 1);
+    }
+}
